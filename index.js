4760b15405684e6a630fb737541e90947f187b8c
Philipp Nowinski <philipp@nowinski.de>
2015-07-16 07:59:45 +0200

    [BUGFIX] check if file.stat.size is defined

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var map = require('map-stream'),
 	gutil = require('gulp-util'),
-	getFileSize = require("filesize");
+	getFileSize = require('filesize');
 
 module.exports = function(fileSizeLimit){
 	'use strict';
@@ -8,7 +8,7 @@ module.exports = function(fileSizeLimit){
 	return map(function(file,callback){
 		var filenameShort = file.path.split(/\/|\\/).pop(),
 			//Check if file.stat exists (gulp.concat removes it for example)
-			filesize = file.stat ? file.stat.size : Buffer.byteLength(String(file.contents)),
+			filesize = file.stat && file.stat.size ? file.stat.size : Buffer.byteLength(String(file.contents)),
 			formattedFilesize = getFileSize(filesize);
 
 		if (typeof fileSizeLimit !== 'undefined' && filesize > fileSizeLimit) {
@@ -19,8 +19,6 @@ module.exports = function(fileSizeLimit){
 			gutil.log('Size of ' + gutil.colors.cyan(filenameShort) + ': ' + gutil.colors.magenta(formattedFilesize));
 		}
 
-		callback(null,file)
+		callback(null,file);
 	});
-
-
 };
